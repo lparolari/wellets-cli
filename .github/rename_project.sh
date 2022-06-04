1e0b7b68d6f6a7b986089215c09ab3ca3f96be95
lparolari <lparolari@users.noreply.github.com>
2022-06-04 14:22:57 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="lparolari"
+original_name="wellets_cli"
+original_urlname="wellets-cli"
+original_description="Awesome wellets_cli created by lparolari"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv wellets_cli $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
