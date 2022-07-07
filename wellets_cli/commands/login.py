19b0d3aadcee65a13dfaad0b27063b7e5419fdf4
lparolari <luca.parolari23@gmail.com>
2022-07-07 23:08:45 +0200

    chore: align login with inquirerPy

--- wellets_cli/commands/login.py
+++ wellets_cli/commands/login.py
@@ -2,7 +2,7 @@ from getpass import getpass
 from typing import Optional
 
 import click
-
+from InquirerPy import inquirer
 import wellets_cli.api as api
 from wellets_cli.auth import persist_auth
 
@@ -11,8 +11,8 @@ from wellets_cli.auth import persist_auth
 @click.option("--email")
 @click.option("--password")
 def login(email: Optional[str], password: Optional[str]):
-    email = email or input("Email: ")
-    password = password or getpass()
+    email = email or inquirer.text(message="Email").execute()
+    password = password or inquirer.secret(message="Password").execute()
 
     user_session = api.login(email, password)
 
