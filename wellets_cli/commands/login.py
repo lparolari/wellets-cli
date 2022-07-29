4d1a57ccf6405c40d1270baaa88addd8df98ee1b
lparolari <luca.parolari@orobix.com>
2022-07-29 23:41:51 +0200

    feat(login): load credentials from env

--- wellets_cli/commands/login.py
+++ wellets_cli/commands/login.py
@@ -1,4 +1,3 @@
-from getpass import getpass
 from typing import Optional
 
 import click
@@ -6,14 +5,23 @@ from InquirerPy import inquirer
 
 import wellets_cli.api as api
 from wellets_cli.auth import persist_auth
+from wellets_cli.config import Config
 
 
 @click.command()
 @click.option("--email")
 @click.option("--password")
 def login(email: Optional[str], password: Optional[str]):
-    email = email or inquirer.text(message="Email").execute()
-    password = password or inquirer.secret(message="Password").execute()
+    email = (
+        email
+        or Config.api_username
+        or inquirer.text(message="Email").execute()
+    )
+    password = (
+        password
+        or Config.api_password
+        or inquirer.secret(message="Password").execute()
+    )
 
     user_session = api.login(email, password)
 
--- wellets_cli/config.py
+++ wellets_cli/config.py
@@ -3,3 +3,5 @@ import os
 
 class Config:
     api_url = os.getenv("API_URL") or "http://localhost:3333"
+    api_username = os.getenv("API_USERNAME")
+    api_password = os.getenv("API_PASSWORD")
