576e040f04b39bd5b7e649f64e56c4c44256dd05
lparolari <luca.parolari23@gmail.com>
2022-08-15 00:53:28 +0200

    feat(config): prettied show/set commands

--- wellets_cli/commands/config.py
+++ wellets_cli/commands/config.py
@@ -4,9 +4,14 @@ from tabulate import tabulate
 import wellets_cli.api as api
 from wellets_cli.auth import get_auth_token
 from wellets_cli.config import Config
+from wellets_cli.question import currency_question
 from wellets_cli.util import make_headers
 
-from ..question import currency_question
+CONFIG_API_URL = "api.url"
+CONFIG_API_USERNAME = "api.username"
+CONFIG_API_PASSWORD = "api.password"
+CONFIG_USER_PREFERRED_CURRENCY = "user-settings.preferred-currency"
+SETTABLE_CONFIGS = [CONFIG_USER_PREFERRED_CURRENCY]
 
 
 @click.group()
@@ -14,18 +19,20 @@ def config():
     pass
 
 
-@config.command()
+@config.command(name="show")
 @click.option("--auth-token")
-def show(auth_token):
+def show_config(auth_token):
     auth_token = auth_token or get_auth_token()
     headers = make_headers(auth_token)
 
     preferred_currency = api.get_preferred_currency(headers=headers)
 
     data = [
-        {"config": "api_url", "value": Config.api_url},
+        {"config": CONFIG_API_URL, "value": Config.api_url},
+        {"config": CONFIG_API_USERNAME, "value": Config.api_username},
+        {"config": CONFIG_API_PASSWORD, "value": "<sensitive>"},
         {
-            "config": "preferred_currency",
+            "config": CONFIG_USER_PREFERRED_CURRENCY,
             "value": preferred_currency.acronym,
         },
     ]
@@ -33,23 +40,23 @@ def show(auth_token):
     print(tabulate(data, headers="keys"))
 
 
-@config.command(name="set-currency")
-@click.option("--currency-id")
+@config.command(name="set")
+@click.argument("config")
 @click.option("--auth-token")
-def set_currency(currency_id, auth_token):
+def set_config(config, auth_token):
     auth_token = auth_token or get_auth_token()
     headers = make_headers(auth_token)
 
-    preferred_currency = api.get_preferred_currency(headers=headers)
-    currencies = api.get_currencies(headers=headers)
+    if CONFIG_USER_PREFERRED_CURRENCY in config:
+        preferred_currency = api.get_preferred_currency(headers=headers)
+        currencies = api.get_currencies(headers=headers)
 
-    currency_id = (
-        currency_id
-        or currency_question(currencies, default=preferred_currency).execute()
-    )
+        currency_id = currency_question(
+            currencies, default=preferred_currency
+        ).execute()
 
-    user_settings = api.set_preferred_currency(
-        data={"currency_id": currency_id}, headers=headers
-    )
+        user_settings = api.set_preferred_currency(
+            data={"currency_id": currency_id}, headers=headers
+        )
 
     print(user_settings.id)
