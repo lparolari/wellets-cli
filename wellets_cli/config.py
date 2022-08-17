1624d9f6b11bc418a45a1d4f716d035d6bb968ff
lparolari <luca.parolari23@gmail.com>
2022-08-17 17:40:10 +0200

    chore: update defaults

--- wellets_cli/config.py
+++ wellets_cli/config.py
@@ -2,6 +2,6 @@ import os
 
 
 class Config:
-    api_url = os.getenv("API_URL") or "http://localhost:3333"
+    api_url = os.getenv("API_URL") or "https://wellets-backend.herokuapp.com"
     api_username = os.getenv("API_USERNAME")
     api_password = os.getenv("API_PASSWORD")
