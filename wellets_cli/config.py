fc1b6904305c12bac79f24a56609d59db9bd6f18
lparolari <luca.parolari23@gmail.com>
2023-04-23 20:55:29 +0200

    chore: update configs with env

--- wellets_cli/config.py
+++ wellets_cli/config.py
@@ -1,9 +1,14 @@
 
-from dynaconf import Dynaconf
+from dynaconf import Dynaconf, Validator
 
 settings = Dynaconf(
-    envvar_prefix="DYNACONF",
+    envvar_prefix="WELLETS",
     settings_files=['settings.toml', '.secrets.toml'],
+    environments=True,
+    env_switcher="WELLETS_ENV",
+    validators=[
+        Validator("api_url", required=True),
+    ]
 )
 
 # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
